4b123a9c8c07cef8c1659c0f1eb96a9955c9d38b
Trevor <toantu3005@gmail.com>
2024-07-20 19:42:04 +1000

    fix: remove extra slashes in the weather api base url

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
-const baseUrl = '/api/weather';
+const baseUrl = 'api/weather';
 
 const getWeather = async () => {
     const response = await axios.get(`${apiUrl}/${baseUrl}`);
