33a5426cf3bbe2b3cad57e8733138ac4d83c7620
Trevor <toantu3005@gmail.com>
2024-07-20 11:57:55 +1000

    build: add env.development and .env.production to the correct backend urls

--- src/services/weather.ts
+++ src/services/weather.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
 const baseUrl = '/api/weather';
 
 const getWeather = async () => {
-    const response = await axios.get(baseUrl);
+    const response = await axios.get(`${apiUrl}/${baseUrl}`);
     return response.data;
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,6 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
       },
-      '/api/weather': {
-        target: 'https://thanhtoantu-portfolio-backend.fly.dev/',
-        changeOrigin: true,
-      }
     },
   },
   base: '/thanhtoantu-portfolio-frontend/',
